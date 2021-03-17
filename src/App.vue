673ac4780faf0669ab75543a9e3decf60fae33ff
Caleb Mackey <caleb.mackey@gmail.com>
2021-03-17 11:35:53 -0500

    fix(App.vue): change from reactive to static data properties

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,79 @@
 <template>
-  <div id="esri-map"></div>
+  <div id="esri-map" ref="esriMap"></div>
 </template>
 
 <script>
 import Map from "@arcgis/core/Map";
 import MapView from "@arcgis/core/views/MapView";
 import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
-import { ref, onMounted } from "vue";
+import { defineComponent, nextTick } from "vue";
 
-export default {
+
+export default defineComponent({
+  
   setup() {
-    let layer = ref(null);
-    let view = ref(null);
+    /**
+     * do we really want vue to make these reactive? Are they really going to change?
+     * this will add extra overhead. If we need to handle reactive changes on esri stuff,
+     * we can take advantage by using "esri/core/watchUtils" or by using the Accessor.watch()
+     */
+    // let layer = ref(null);
+    // let view = ref(null);
 
     //same issues with reactive object as well
     ///let app = reactive({ zones: null, view:null });
+    
+    // instead declare them as static data properties
+    const map = new Map({
+      basemap: "topo-vector",
+    });
 
-    const setupMap = () => {
-      const map = new Map({
-        basemap: "topo-vector",
-      });
+    const view = new MapView({
+      container: "esri-map", // just pass this here to set required property
+      map,
+      zoom: 5,
+      center: [-95, 39],
+      popup: {
+        autoOpenEnabled: false,
+      },
+    });
 
-      view.value = new MapView({
-        container: "esri-map",
-        map,
-        zoom: 5,
-        center: [-95, 39],
-        popup: {
-          autoOpenEnabled: false,
-        },
-      });
+    const layer = new FeatureLayer({
+      portalItem: {
+        id: "9e2f2b544c954fda9cd13b7f3e6eebce",
+      },
+      outFields: ["*"],
+      title: "Recent Earthquakes",
+    });
 
-      layer.value = new FeatureLayer({
-        portalItem: {
-          id: "9e2f2b544c954fda9cd13b7f3e6eebce",
-        },
-        outFields: ["*"],
-        title: "Recent Earthquakes",
-      });
+    map.add(layer)
 
-      try {
-        //this will generate an error
-        map.add(layer.value);
-      } catch (e) {
-        console.log("error with map.add(layer)");
-        console.log(e);
-        map.add(layer.value.load());
-      }
+    return {
+      view,
+      map,
+      layer
+    }
 
-      //this will also throw an error
-      view.value.whenLayerView(layer.value).then((layerView) => {
-        console.log("layerview generated");
-        console.log(layerView);
-      });
-    };
-    onMounted(setupMap);
-    return { setupMap, view, layer };
   },
-};
+
+  created(){
+    // verify we have access to view, layer
+    console.log('view: ', this.view)
+    console.log('map: ', this.map)
+    
+    /**
+     * the map actually won't render from our setup() because the
+     * "esri-map" div does not exist yet, so we must re-set it here 
+     */
+    nextTick(()=> this.view.container = this.$refs.esriMap)
+
+    // here is where we can actually test things out
+    this.view.whenLayerView(this.layer).then((layerView) => {
+      console.log("layerview generated");
+      console.log(layerView);
+    });
+  }
+});
 </script>
 
 <style scoped>
